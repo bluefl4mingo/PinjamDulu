5521232f16044ea8490d2c4389de9cd32eadec31
bluefl4mingo <ramadhanifebruarrahman@mail.ugm.ac.id>
2024-09-02 15:19:54 +0700

    Add Classes

---
+++ PinjamDuluApp/Models/Gadget.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PinjamDuluApp.Models
+{
+    internal class Gadget
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Condition { get; set; }
+        public int RentalPrice { get; set; }
+        public bool Availability { get; set; }
+        public DateTime AvailabilityDate {  get; set; }
+        public int Images { get; set; }
+
+        public Gadget(string title, string desctiption)
+        {
+            Title = title;
+            Description = desctiption;
+        }
+    }
+}
---
+++ PinjamDuluApp/Models/Review.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.Pkcs;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PinjamDuluApp.Models
+{
+    internal class Review
+    {
+        public string Id { get; set; }
+        public string GadgetID { get; set; }
+        public string LenderID { get; set; }
+        public string RenterID { get; set; }
+        public int Rating { get; set; }
+        public string ReviewText { get; set; }
+        public DateTime ReviewDate { get; set; }
+
+        public Review()
+        {
+
+        }
+
+    }
+}
