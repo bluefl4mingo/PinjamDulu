92475a7e4303999ede3ca96ae24ed820916fd8fe
bluefl4mingo <ramadhanifebruarrahman@mail.ugm.ac.id>
2024-09-08 17:32:52 +0700

    Update 'Model' classes and adding booking conlict exceptions

--- PinjamDuluApp/App.xaml.cs
+++ PinjamDuluApp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using PinjamDuluApp.Models;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +10,11 @@ namespace PinjamDuluApp
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            User user = new User("Adzka");
+            base.OnStartup(e);
+        }
     }
 
 }
